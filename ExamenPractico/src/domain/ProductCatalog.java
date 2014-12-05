f342a3e5f27b882fa74d4cb2ad55c005301673bf
César Higuera <rasec_navi1@hotmail.com>
2014-12-04 17:22:16 -0700

    ref-3 Se facilitó la comprensión del constructor de la clase ProductCatalog delegando la responsabilidad al método generateSpecifications.

--- ExamenPractico/src/domain/ProductCatalog.java
+++ ExamenPractico/src/domain/ProductCatalog.java
@@ -6,12 +6,17 @@ public class ProductCatalog {
   private Hashtable productSpecifications = new Hashtable();
 
   public ProductCatalog() {
-	  ProductSpecification ps;
-	  for (int i = 1 ; i <= 4 ; i++ ){
+	  generateSpecifications();
+  }
+
+  //Genera las especificaciones de los productos
+private void generateSpecifications() {
+	ProductSpecification ps;
+	for (int i = 1 ; i <= 4 ; i++ ){
 		  ps = new ProductSpecification(i, i*100, "product " + i);
 		  productSpecifications.put(new Integer(i), ps);
 	  }
-  }
+}
 
   public ProductSpecification specification(int upc) {
     return (ProductSpecification) productSpecifications.get(new Integer(upc));
